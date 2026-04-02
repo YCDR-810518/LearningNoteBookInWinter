930c7cc67d3b701bff0a58cb4c5aced733d4f498
YCDR-810518 <yaochen1234567@outlook.com>
2026-04-02 15:31:47 +0800

    进行了一点点开发

--- ProjectCode/QG_data_analysis/main.py
+++ ProjectCode/QG_data_analysis/main.py
@@ -9,9 +9,15 @@ with open('QG_train.csv', 'r', encoding='utf-8') as f:
 
     # 数据预处理
     # 提取特征值
-    data_feature =
+    data_feature = data.drop('target', axis=1).copy()
+
     # 提取标签值
     data_label = data['target'].copy()
+
+    # 查看是否成功去除无用值
     print(data_feature.head())
     print(data_label.head())
+
+    # 成功去除无用值
+    # 接下来进行模型训练
 
